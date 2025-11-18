3a08bd661958ce617bde5d05b80c0b89cce227d7
Demon-ut <anthony.goasdoue@ynov.com>
2025-11-18 12:20:13 +0100

    V2

--- esp32/Radar/src/main.cpp
+++ esp32/Radar/src/main.cpp
@@ -1,49 +1,127 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
+#define ULTRASON
+#define WIFICLIENT
+#define SERVOMOTEUR
 
-#define PIN_SG90 13
-#define TRIG_PIN 27
-#define ECHO_PIN 26
+#if defined(WIFICLIENT)
+  #include <WiFi.h>
+  const char* ssid = "Bigo";
+  const char* mdp = "pipicaca";
+#endif
 
-Servo sg90;
 
-long duration;
-int distance;
+#if defined(MQTTCLIENT)
+  #include <PubSubClient.h>
+#endif
 
+
+#if defined(SERVOMOTEUR)
+  #include <ESP32Servo.h>
+  #define PIN_SG90 13
+  Servo radarServo;
+#endif
+
+#if defined(ULTRASON)  
+  #define TRIG_PIN 27
+  #define ECHO_PIN 26
+
+  long duration;
+  int distance;
+#endif
+
+#if defined(WIFICLIENT)
+  /**
+   * @brief Initialise et connecte l'ESP32 au réseau WiFi.
+   *
+   * @details
+   * Démarre la connexion WiFi en utilisant les identifiants globaux `ssid` et `mdp`.
+   * Affiche des informations sur le moniteur série :
+   *  - affiche le nom du réseau visé,
+   *  - confirme la connexion.
+   */
+  void setupWifi() {
+    delay(100);
+    Serial.print("\nConnexion a :");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, mdp);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(100);
+      Serial.print("-");
+    }
+    
+    Serial.print("\nConnecte a :");
+    Serial.println(ssid);
+  }
+#endif
+
+
+/**
+ * @brief Initialise les différents composant de l'ESP32
+ * 
+ * @details 
+ * 
+ */
 void setup() {
   Serial.begin(115200);
-  sg90.attach(PIN_SG90, 500, 2400);
+
+#if defined(WIFICLIENT)
+  setupWifi();
+#endif
+
+#if defined(SERVOMOTEUR)  
+  radarServo.attach(PIN_SG90);
+  Serial.println("Test SG90");
+#endif
+
+  #if defined(ULTRASON)  
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-  Serial.println("Test SG90 + HC-SR04");
+  Serial.println("Test HC-SR04");
+#endif
 }
 
-int angle = 0;
-int step = 1; // sens du servo
+#if defined(SERVOMOTEUR)  
+  int angle = 0;
+  int step = 1; 
+  int direction = 1;
+#endif
 
 void loop() {
   // --- Boucle servo de 0° à 180° et retour ---
-  angle += step;
-  if (angle >= 180 || angle <= 0) step = -step; // inverse le sens
-  sg90.write(angle);
+  #if defined(SERVOMOTEUR)  
+    radarServo.write(angle);
+    delay(15); // ajuster vitesse
 
-  // --- Lecture distance HC-SR04 ---
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+    angle += step * direction;
 
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = duration * 0.034 / 2; // distance en cm
+    if(angle >= 180) direction = -1;
+    if(angle <= 0) direction = 1;   
 
-  Serial.print("Angle servo: ");
-  Serial.print(angle);
-  Serial.print("° - Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+    Serial.print("servo angle: ");
+    Serial.println(angle);  
+  #endif
 
-  delay(20); // contrôle vitesse servo
+  #if defined(ULTRASON)  
+    // --- Lecture distance HC-SR04 ---
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = duration * 0.034 / 2; 
+
+    Serial.print("° - Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+  #endif
+
+  // controle vitesse servo
+  // delay(1000); 
 }
 
 
